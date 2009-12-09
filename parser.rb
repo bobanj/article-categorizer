f484873faa83ceea590b1e76823deaff9433d2c1
Boban <bobanj@gmail.com>
2009-12-09 19:36:48 +0100

    added KCODE update

--- parser.rb
+++ parser.rb
@@ -3,18 +3,19 @@ require 'nokogiri'
 require 'net/http'
 
 SITE = 'www.a1.com.mk'
-
+$KCODE='UTF8'
 categories = []
 http = Net::HTTP.new(SITE)
 
 #response.each{|k,v| puts "#{k} => #{v}"}
 #encoding = response["content-type"].match(/charset=(\S+)/)
-(50..1000).each do |num|
+(50..100).each do |num|
   response, doc = http.get("/vesti/default.aspx?VestID=#{num}")
   if response.code == "200"
     doc = Nokogiri::HTML(doc, nil, 'WINDOWS-1251')
-    category = doc.css('a#lnkKat')[0].content
+    category = doc.css('a#lnkKat')[0].content.strip
     categories << category unless categories.include? category
   end
 end
 p categories
+puts categories.size
