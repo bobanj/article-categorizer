26d5040d93862830c1671f8b3bec6a8ce6d34fff
Boban Jovanoski <bobanj@ubuntu-laptop.(none)>
2009-12-14 15:17:10 +0100

    fix for media rake task

---
+++ lib/media.rb
@@ -0,0 +1,58 @@
+#if start and count params are provided parsing is done from the provided
+#startid, and if count is the number of articles to parse
+class Media
+  def initialize(name, start, count)
+    @portal = Portal.find_by_name(name)
+    @http = Net::HTTP.new(@portal.hostname)
+    @first_article_id = get_first_article_id(start)
+    @max_article_id = get_max_article_id
+    @last_article_id = get_last_article_id(count)
+  end
+
+  #Gets all requested articles from www.a1.com.mk
+  def get_articles
+    puts "FIRST ARTICLE ID #{@first_article_id}"
+    puts "LAST ARTICLE ID #{@last_article_id}"
+    (@first_article_id..@last_article_id).each do |article_id|
+      response, doc = @http.get("/vesti/default.aspx?VestID=#{article_id}")
+      if response.code == "200"
+        doc = Nokogiri::HTML(doc, nil, 'WINDOWS-1251')
+        category_name = doc.at('a#lnkKat').text.strip
+        category = @portal.categories.find_or_create_by_name(:name => category_name)
+        title = doc.at('#H2naslov').text
+        body = title + doc.css('td p').collect(&:text).join
+        category.articles.create(:title => title, :body => body, :itemid => article_id.to_s)
+        puts "Article #{article_id}"
+      end
+    end
+  end
+
+  #Private functions go here
+  private
+  def get_first_article_id(start)
+    if start.blank?
+      @first_article_id = @portal.articles.last.itemid.to_i rescue 50
+    else
+      @first_article_id = (start.to_i > 50 ? start.to_i : 50)
+    end
+  end
+
+  def get_last_article_id(count)
+    if count.blank?
+      @max_article_id
+    else
+      ((@first_article_id + count.to_i) > @max_article_id ? @max_article_id : (@first_article_id + count.to_i))
+    end
+  end
+
+  def get_max_article_id
+    response, doc = @http.get("/default.aspx")
+    if response.code == "200"
+      doc = Nokogiri::HTML(doc, nil, 'WINDOWS-1251')
+      doc.at('h2>a.Vesti')[:href].match(/[0-9]+/).to_s.to_i rescue 0
+    else
+      puts "#------ RESPONSE CODE FOR /default.aspx FAIL ------#"
+      return 0
+    end
+  end
+end
--- lib/tasks/get_articles.rake
+++ lib/tasks/get_articles.rake
@@ -1,8 +1,8 @@
 #This rake task gets articles from www.a1.com.mk and stores them in database
-namespace :a1 do
+namespace :media do
   desc "Fill database with articles from a1.com.mk"
   task :get_articles => :environment do
-    a1 = Media::A1.new(ENV['start'],ENV['count'])
-    a1.get_articles
+    media = Media.new("a1",ENV['start'],ENV['count'])
+    media.get_articles
   end
 end
